e006e43d00a1d93d53e5684e8f6cfb68d45d247f
Erin Millard <contact@erin.id.au>
2025-07-21 14:29:52 +1000

    Add bundle analyzer

--- next.config.ts
+++ next.config.ts
@@ -1,3 +1,4 @@
+import bundleAnalyzer from "@next/bundle-analyzer";
 import type { NextConfig } from "next";
 
 const nextConfig: NextConfig = {
@@ -19,4 +20,10 @@ const nextConfig: NextConfig = {
   },
 };
 
-export default nextConfig;
+export default withBundleAnalyzer(nextConfig);
+
+function withBundleAnalyzer(config: NextConfig): NextConfig {
+  return process.env.ANALYZE === "true"
+    ? bundleAnalyzer({ openAnalyzer: false })(config)
+    : config;
+}
